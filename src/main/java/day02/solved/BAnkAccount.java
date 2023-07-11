02dc792f6a59ba8ba412a47aa271f9ff9d90c4c8
vanitha-velu <vanitha.velu@fssa.freshworks.com>
2023-07-11 16:47:28 +0530

    Day02

---
+++ src/main/java/day02/solved/BAnkAccount.java
@@ -0,0 +1,33 @@
+package day02.solved;
+
+public class BAnkAccount {
+	
+	private String accNo;
+	private String name;
+	private double balance;
+
+	public String getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+}
