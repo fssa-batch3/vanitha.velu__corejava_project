bd0a4785fd9deb298bf15f2cf6685d704d08b3d8
vanitha-velu <vanitha.velu@fssa.freshworks.com>
2023-07-19 09:16:30 +0530

    day07

--- src/main/java/day06/practice/Task.java
+++ src/main/java/day06/practice/Task.java
@@ -1,2 +1,36 @@
 package day06.practice;
 
+import java.util.ArrayList;
+
+class addTask {
+	private int taskId;
+	private String taskName;
+	private String description;
+
+	// Constructor
+	public addTask(int taskId, String taskName, String description) {
+		this.taskId = taskId;
+		this.taskName = taskName;
+		this.description = description;
+	} 
+
+	@Override
+	public String toString() {
+		return "Task ID: " + taskId + ", Task Name: " + taskName + ", Description: " + description;
+	} 
+} 
+
+public class Task {
+	public static void main(String[] args) {
+
+		ArrayList<addTask> taskList = new ArrayList<>();
+
+		taskList.add(new addTask(1, "Finish report", "Complete the quarterly sales report."));
+		taskList.add(new addTask(2, "Buy groceries", "Purchase items for the week."));
+		taskList.add(new addTask(3, "Study for exam", "Prepare for the upcoming Java exam."));
+
+		for (addTask task : taskList) {
+			System.out.println(task);
+		}
+	}
+}
